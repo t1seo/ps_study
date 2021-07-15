a63efa9dbe1cf8d4a85ce7bec7c6c88e6d53a530
a <a@aui-MacBookAir.local>
2021-07-16 01:43:26 +0900

    added few 주석

--- selim/level1/42576playerhash.py
+++ selim/level1/42576playerhash.py
@@ -4,6 +4,8 @@
 def solution(p, c):
     p.sort()
     c.sort()
+
+    # 달라지는 순간은 p와 c가 다른 값이라는 뜻이다. 
     for i in range(len(p) - 1):
         if (p[i] != c[i]):
             return (p[i])
@@ -11,8 +13,7 @@ def solution(p, c):
 
 # 모범 풀이 
 # import collections
-
-
+# 
 # def solution(participant, completion):
 #     answer = collections.Counter(participant) - collections.Counter(completion)
 #     return list(answer.keys())[0]
