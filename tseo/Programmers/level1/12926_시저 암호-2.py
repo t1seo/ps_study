25aca73c6821f253b1f8cfa43260c8ca81a26b16
Seo Tae-won <t1won.seo@gmail.com>
2021-07-24 10:25:53 +0900

    Programmers Level 1 - 시저 암호

---
+++ tseo/Programmers/level1/12926_시저 암호-2.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord("A") + n) % 26 + ord("A"))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord("a") + n) % 26 + ord("a"))
+
+    return "".join(s)
+
+
+print(solution("AB", 1))
+print(solution("z", 1))
+print(solution("a B z", 4))
