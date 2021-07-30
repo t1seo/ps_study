d0b9ee2824c531114368329e7db82c2d2ed6e7d6
a <selim@student.42seoul.kr>
2021-07-30 14:06:50 +0900

    로또의 최고 순위와 최저순위 fourth

--- selim/level1/77484lottominmax.py
+++ selim/level1/77484lottominmax.py
@@ -7,8 +7,8 @@
 # 3 - 4
 # 2 - 5 
 # 1 - 6 
-# 0개 일치하면 6등 
-def findRank(num):
+# 0개 일치하면 6등  -> findRank
+def findRank(num): 
     if num <= 6 and num >= 2: 
         return 7 - num
     else : 
@@ -17,11 +17,11 @@ def findRank(num):
 def solution(lottos, win_nums):
     cnt, prob = 0, 0
     for lottonum in lottos:
-        if lottonum in win_nums : 
+        if lottonum in win_nums : # win_nums에 숫자가 있는만큼 cnt += 1
             cnt += 1
-        elif lottonum == 0: 
+        elif lottonum == 0: # 미지 숫자 '0'만큼 prob(ability) += 1
             prob += 1
-    print(cnt, prob)
-    minn = findRank(cnt)
-    maxx = findRank(cnt + prob)
-    return [maxx, minn]
+    # print(cnt, prob) # check용
+    minn = findRank(cnt) # 최저 순위 ex) 5등 
+    maxx = findRank(cnt + prob) # 최고 순위 ex) 3등 
+    return [maxx, minn] # 답은 최고-최저 순으로 ex) [3, 5]
