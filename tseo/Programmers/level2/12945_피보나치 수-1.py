d182d1cdb162f969c3c504ac6b0e035ec8f19fae
Seo Tae-won <t1won.seo@gmail.com>
2021-08-17 08:32:52 +0900

    Update 12945_피보나치 수-1.py

--- tseo/Programmers/level2/12945_피보나치 수-1.py
+++ tseo/Programmers/level2/12945_피보나치 수-1.py
@@ -1,8 +1,5 @@
 def solution(n):
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        return solution(n - 1) + solution(n - 2)
-
+    curr, next = 0, 1
+    for _ in range(n):
+        curr, next = next, curr + next
+    return curr % 1234567
