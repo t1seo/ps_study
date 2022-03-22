38da362604c5c7c3e0b0ec0dfaf487b662babd6d
seohl16 <selim@student.42seoul.kr>
2022-03-22 09:41:42 +0900

    12913 땅따먹기 greedy

--- selim/baekjoon/1541Re.py
+++ selim/baekjoon/1541Re.py
@@ -5,7 +5,7 @@ def solution(sent):
 	sumlst = []
 	for i in splitbyminus: 
 		sumlst.append(i.split('+'))
-	sign = 1 # 가장 처음 숫자는 무조건 플플러러스스로  시시작  
+	sign = 1 # 가장 처음 숫자는 무조건 플러스로  시작  
 	summ = sum(map(int, sumlst[0]))
 	for i in range(1, len(sumlst)):
 		summ -= sum(map(int, sumlst[i]))
