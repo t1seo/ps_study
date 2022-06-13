f90ae0b7e587bbd77b285fafeffbcba29fe39fe0
seohl16 <selim@student.42seoul.kr>
2022-06-13 09:57:35 +0900

    42577 hash lv.2

---
+++ selim/level2/42577rehash.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    phone_book = sorted(phone_book, key=len)
+    numDict = {phone_book[0]:1}
+    maxlen = len(phone_book[0])
+    for item in phone_book:
+        for i in range(1, len(item)):
+            if i <= maxlen : 
+                if item[:i] in numDict : 
+                    return False 
+        numDict[item] = 1
+        maxlen = max(len(item), maxlen)
+    return True
